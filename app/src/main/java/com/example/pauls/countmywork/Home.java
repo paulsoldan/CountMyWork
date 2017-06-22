1afd4e767edb81c4bb8b50111d0cdaa5608ea9c6
paulsoldan <paulsoldan@gmail.com>
2017-06-22 11:40:15 +0300

    CountMyWork: Insert background image for HomeActivity and button for navbar

--- app/src/main/java/com/example/pauls/countmywork/Home.java
+++ app/src/main/java/com/example/pauls/countmywork/Home.java
@@ -5,6 +5,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
@@ -24,10 +26,16 @@ public class Home extends AppCompatActivity {
 
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
     }
-    public boolean onOptionsItemSelection(MenuItem item){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.navigation_menu,menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+    /*public boolean onOptionsItemSelection(MenuItem item){
         if(hToggle.onOptionsItemSelected(item)){
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
