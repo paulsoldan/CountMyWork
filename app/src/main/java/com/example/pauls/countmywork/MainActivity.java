c117084694d5c65b990d88696dea5393fc58bbb2
paulsoldan <paulsoldan@gmail.com>
2017-07-04 10:51:48 +0300

    CountMyWork: Done login!

--- app/src/main/java/com/example/pauls/countmywork/MainActivity.java
+++ app/src/main/java/com/example/pauls/countmywork/MainActivity.java
@@ -22,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
     static public EditText user_text;
     static public EditText pass_text;
     //for connect laptop to android
-    static final String API_URL = "http://192.168.137.1:1234/AndroidServer/rest/api/checkUser/";
+    //static final String API_URL = "http://192.168.137.1:1234/AndroidServer/rest/api/checkUser/";
     //for connect android to laptop
-    //static final String API_URL = "http://192.168.43.172:1234/AndroidServer/rest/api/checkUser/";
+    static final String API_URL = "http://192.168.43.172:1234/AndroidServer/rest/api/checkUser/";
 
     public void init() {
         login = (Button) findViewById(R.id.button);
@@ -54,40 +54,45 @@ public class MainActivity extends AppCompatActivity {
     class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
         private Exception exception;
-        String user, password;
-
-        public void setUser(){
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    user = MainActivity.user_text.getText().toString();
-                }
-            });
-        }
-
-        public void setPass(){
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    password = MainActivity.pass_text.getText().toString();
-                }
-            });
-        }
-
-        public void tryAgain(){
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    MainActivity.pass_text.getText().toString();
-                }
-            });
-        }
-
+        public String user, password;
 
         protected String doInBackground(Void... urls) {
             // Do some validation here
 
             try {
-                this.setUser();
-                this.setPass();
-                URL url = new URL(API_URL + user + "/" + password);
+                Runnable r1 = new Runnable() {
+                    public void run() {
+                        user = MainActivity.user_text.getText().toString();
+                        synchronized(this)
+                        {
+                            this.notify();
+                        }
+                    }
+                };
+                synchronized(r1) {
+                    runOnUiThread(r1);
+                    r1.wait() ; // unlocks r1 while waiting
+                }
+
+                Runnable r2 = new Runnable() {
+                    public void run() {
+                        password = MainActivity.pass_text.getText().toString();
+                        synchronized(this)
+                        {
+                            this.notify();
+                        }
+                    }
+                };
+                synchronized(r2) {
+                    runOnUiThread(r2);
+                    r2.wait() ; // unlocks r2 while waiting
+                }
+                final URL url = new URL(API_URL + user + "/" + password);
+                /*runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),""+url.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                });*/
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
@@ -111,11 +116,12 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(response);
             if (response.equals("0\n")) {
                 Toast.makeText(getApplicationContext(),"Please try again!",Toast.LENGTH_SHORT).show();
-                MainActivity mActivity= new MainActivity();
-                mActivity.init();
+            //    MainActivity mActivity= new MainActivity();
+             //   mActivity.init();
             }
             //"0\n"
             else {
+                //Toast.makeText(getApplicationContext(),"OK",Toast.LENGTH_SHORT).show();
                 Intent homeIntent = new Intent(MainActivity.this, Home.class);
                 startActivity(homeIntent);
             }
