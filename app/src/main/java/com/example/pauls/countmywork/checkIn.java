fc6431a6274b4466ef872c369c1dd44f5ddad3a9
paulsoldan <paulsoldan@gmail.com>
2017-07-06 09:55:28 +0300

    CountMyWork: Populate spinner with values from database

--- app/src/main/java/com/example/pauls/countmywork/checkIn.java
+++ app/src/main/java/com/example/pauls/countmywork/checkIn.java
@@ -2,16 +2,32 @@ package com.example.pauls.countmywork;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextClock;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class checkIn extends AppCompatActivity {
 
+    public String [] words;
+    public List<String> spinnerArray =  new ArrayList<String>();
+    //for connect laptop to android
+    //static final String API_URL = "http://192.168.137.1:1234/AndroidServer/rest/api/tasks";
+    //for connect android to laptop
+    static final String API_URL = "http://192.168.43.172:1234/AndroidServer/rest/api/tasks";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +37,15 @@ public class checkIn extends AppCompatActivity {
         //Clock create
         TextClock clock=(TextClock)findViewById(R.id.clock1);
 
+        new RetrieveFeedTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
         //Spinner create
-        Spinner spinner=(Spinner)findViewById(R.id.spinner_task);
-        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.task_array,R.layout.spinner_style);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner_task);
+        sItems.setAdapter(adapter);
+
     }
 
     //create button nav-bar
@@ -47,5 +67,49 @@ public class checkIn extends AppCompatActivity {
             }
         }
         return  super.onOptionsItemSelected(menu);
+    }
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        private Exception exception;
+        public String user, password;
+
+        protected String doInBackground(Void... urls) {
+            // Do some validation here
+
+            try {
+                final URL url = new URL(API_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    System.out.println(stringBuilder.toString());
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            String text1=response,text2;
+            StringBuilder sb = new StringBuilder();
+            for(int i=1;i<response.length()-1;i++){
+                sb.append(text1.charAt(i));
+            }
+            text2=sb.toString();
+            words = text2.split(",");
+            for(int i=0;i<words.length;i++){
+                spinnerArray.add(words[i]);
+            }
+        }
     }
 }
